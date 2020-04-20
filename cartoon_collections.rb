444911cebd698f2711b105dc65ede77e619b12d5
Josh Beglan <josh_beglan@yahoo.com>
2020-04-20 23:05:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   
-  array.each_with_index do |el|
-    puts el
+  array.each_with_index do |el, i|
+    puts i + ". " + el
   end
 end
